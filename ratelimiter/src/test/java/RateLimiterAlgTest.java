7766bfe0607fca043dc477b9c9b5a901aab7f060
noah2021 <noah2021@foxmail.com>
2022-10-09 23:37:02 +0800

    :bookmark: 修改测试用例参数

--- ratelimiter/src/test/java/RateLimiterAlgTest.java
+++ ratelimiter/src/test/java/RateLimiterAlgTest.java
@@ -21,9 +21,8 @@ public class RateLimiterAlgTest {
 
     @Test
     public void fixedTimeWindowRateLimiterTest() throws RateLimiterException, InterruptedException {
-        RateLimitAlg fixedTimeWindowRateLimiter = new FixedTimeWindowRateLimiter(1);
+        RateLimitAlg fixedTimeWindowRateLimiter = new FixedTimeWindowRateLimiter(3);
         while (true) {
-            // 在访问该方法之前首先要进行 RateLimiter 的获取，返回值为实际的获取等待开销时间
             System.out.println(Thread.currentThread() + ": elapsed seconds " + LocalDateTime.now() + " tryAcquire ->" + fixedTimeWindowRateLimiter.tryAcquire());
             Thread.sleep(200);
         }
@@ -31,9 +30,8 @@ public class RateLimiterAlgTest {
 
     @Test
     public void leakyBucketLimiterTest() throws RateLimiterException {
-        RateLimitAlg leakyBucketLimiter = new LeakyBucketLimiter(10, 1);
+        RateLimitAlg leakyBucketLimiter = new LeakyBucketLimiter(10, 0.5);
         while (true) {
-            // 在访问该方法之前首先要进行 RateLimiter 的获取，返回值为实际的获取等待开销时间
             leakyBucketLimiter.tryAcquire();
             System.out.println(Thread.currentThread() + ": elapsed seconds " + LocalDateTime.now());
         }
